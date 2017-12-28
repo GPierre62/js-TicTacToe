ab1a97ad77c3868fa851bf229bea3bb046ae9825
GPierre62 <godissart@gmail.com>
2017-12-28 12:30:13 +0100

    placement de la div informative (score, nbtour, et tourjoueur)

--- js/TTT.js
+++ js/TTT.js
@@ -1,19 +1,27 @@
 var player=1;
-function joueur(block) {
+var tourJoueur= "joueur 1";
+var nbTours=0;
+document.getElementById('tourEtNbTour').document.write("c'est au tour du "+tourJoueur+" et c'est le tour "+nbTours);
+function switchJoueur(block) {/*
 /*  var player1=block.style.backgroundColor="green";
   var player2=block.style.backgroundColor="blue";*/
 
   if (player==1){
   block.setAttribute("class", "play1");
     player=2;
-    console.log ("joueur 1");
-
+      /*console.log ("joueur 1");*/
+    tourJoueur= "joueur 2";
+    nbTours++;
+    console.log (nbTours);
   }
 
   else if (player==2){
     block.setAttribute("class", "play2");
     player=1;
-    console.log ("joueur 2");
+      /*console.log ("joueur 2");*/
+    tourJoueur= "joueur 1";
+    nbTours++;
+    console.log (nbTours);
   }
 
   block.onclick=function(){
@@ -29,7 +37,9 @@ var totalCases = lignes * colonnes;
 
 // init grille
 for(i = 0; i < totalCases; i++) {
-	grille.push(i);}
+	grille.push(i);
+  }
+
   document.write("<table>");
   // on affiche avec une double boucle pour l'effet grille
   for(j = 0; j < lignes; j++) {
@@ -37,7 +47,7 @@ for(i = 0; i < totalCases; i++) {
 	document.write("<tr>");
 
 	for(k = 0; k < colonnes; k++) {
-		document.write("<td class='classBase'onclick='joueur(this);'><strong>");
+		document.write("<td class='classBase'onclick='switchJoueur(this);'><strong>");
 		document.write(grille[k + (j*colonnes)]);
 		document.write("</strong></td>");
 	}
